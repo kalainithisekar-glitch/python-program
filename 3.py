71f503b4e7d82ed2d6ab3a2c2115bb641e950c7e
kalainithisekar-glitch <kalainithisekar@gmail.com>
2026-08-18 19:00:48 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,17 @@
+a="welcome"
+b=2
+c=0.2
+d=3+5j
+e=[2,4,6,8]
+s1=(1,)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(s1))
+print(type(True))
+print(type(False))
+
+
