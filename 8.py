b8e1c65e70e06e669098102db57771df545c96aa
kalainithisekar-glitch <kalainithisekar@gmail.com>
2026-08-18 19:46:15 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,17 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+operator = input("Enter operation (+, -, *, /): ")
+
+if operator == "+":
+    print("Result =", a + b)
+elif operator == "-":
+    print("Result =", a - b)
+elif operator == "*":
+    print("Result =", a * b)
+elif operator == "/":
+    if b != 0:
+        print("Result =", a / b)
+    else:
+        print("Cannot divide by zero")
+else:
+    print("Invalid operation")
